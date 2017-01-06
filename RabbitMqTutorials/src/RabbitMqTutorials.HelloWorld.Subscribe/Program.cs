91ccef47235c6a510d614d0903955fc60a3c5055
Brian Gardner <brian.gardner@socraticdev.com>
2017-01-06 10:24:05 -0500

    Initial Dev.

---
+++ RabbitMqTutorials/src/RabbitMqTutorials.HelloWorld.Subscribe/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqTutorials.HelloWorld.Subscribe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                channel.BasicQos(0,1,false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    var sleepTime = message.Split('.').Length - 1;
+                    Thread.Sleep(sleepTime * 1000);
+                    Console.WriteLine(" [x] Done proccessing: {0}", message);
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     noAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMqTutorials/src/RabbitMqTutorials/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMqTutorials.HelloWorld.Publish
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    Console.WriteLine("Enter text to send.  . to add 1 second sleep.  Enter blank line to end.");
+                    var input = Console.ReadLine();
+                    while (input != String.Empty)
+                    {
+                        var body = Encoding.UTF8.GetBytes(input);
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "hello",
+                                             basicProperties: properties,
+                                             body: body);
+                        Console.WriteLine(" [x] Sent {0}", input);
+                        input = Console.ReadLine();
+                    }
+                    
+                    
+                }
+            }
+        }
+    }
+}
